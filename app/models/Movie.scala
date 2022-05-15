f3567fe1f1f33f61d53b67e6f01fdcbbb3ac7af7
wilson3849 <wilson3849@gmail.com>
2022-05-15 16:56:24 +0100

    First time sample project loaded for sharing. Movie Data Model is add for startup discuss on project development. Please check the Movie file under app/models directory.

---
+++ app/models/Movie.scala
@@ -0,0 +1,32 @@
+package models
+
+import play.api.libs.json.Json
+
+/**
+ * We use a Scala case class to define our model object
+ * This gives the added benefit of preventing duplicates because
+ * in Scala case classes are said to be equal if all their member variables are equal
+ * See more on here under the title "Comparison"
+ * https://docs.scala-lang.org/tour/case-classes.html
+ *
+ * Movie Class data members collection
+ *  id: String
+ *    follow to IMDb Movie Data Element ID, can extend the function of import data from IMDb API request
+ *  ResultType: String
+ *    data type of record, ie Title
+ *  Image: String
+ *    store URL of the Image
+ *  Title: String
+ *    store Title Name
+ *  Description: String
+ *    store Description of the Movie
+ *  Rating: Int
+ *    store the rating No from 1 to 5, for the update function of the data.
+ *
+ */
+
+case class Movie(id: String, ResultType: String, Image: String, Title: String, Description: String, Rating: Int)
+
+object Movie {
+  implicit val format = Json.format[Movie]
+}
