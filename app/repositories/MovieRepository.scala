a90b2fc0dd3ab5be4223f9de6e885cc3ec4ad6cb
Gurunath <bhuvi23389@gmail.com>
2022-05-17 09:52:58 +0100

    Cleared unwanted code

--- app/repositories/MovieRepository.scala
+++ app/repositories/MovieRepository.scala
@@ -18,28 +18,11 @@ class MovieRepository {
     4
   )
 
-  def getAllMovies: mutable.Set[Movie] = movieList
-
   /**
    * @param movieId The ID of the movie to find
    * @return Option[Movie] An option means this method can return a Movie or optionally a "None"
-   *         https://www.scala-lang.org/api/2.13.8/scala/Option.html
    */
   def getMovie(movieId: Long): Option[Movie] = movieList.collectFirst {
     case movie if movie.id == movieId => movie
   }
-
-//  def addBook(book: Book): Option[Book] = {
-//    // If book already exists then return none
-//    if(bookList.exists(b => b.id == book.id)) {
-//      None
-//    }
-//    else {
-//      // otherwise return the saved after adding it
-//      bookList.addOne(book).collectFirst {
-//        case b if b.id == book.id => b
-//      }
-//    }
-//  }
-
 }
