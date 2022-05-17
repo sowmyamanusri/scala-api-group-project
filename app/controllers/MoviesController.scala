dc23fcc0a98b1ca0ed8ecea7772cd49e9f8d7767
wilson3849 <wilson3849@gmail.com>
2022-05-17 18:10:10 +0100

    UpdateRate function is test and run

---
+++ app/controllers/MoviesController.scala
@@ -0,0 +1,42 @@
+package controllers
+
+import models.Movie
+import play.api.libs.json._
+import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
+import repositories.MovieRepository
+
+import javax.inject.{Inject, Singleton}
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * Movie's home page.
+ */
+@Singleton
+class MoviesController @Inject()(val controllerComponents: ControllerComponents, dataRepository: MovieRepository) extends BaseController {
+
+  def getAll: Action[AnyContent] = Action {
+    try {
+      var movieToReturn = dataRepository.getAllMovies
+      if (movieToReturn.toList.count(id => id != "") == 0) throw new Exception("No Movie is found.")
+      Ok(Json.toJson(movieToReturn))}
+    catch {case ex: Exception => InternalServerError(Json.obj("code" -> INTERNAL_SERVER_ERROR, "message" -> s"get book error : ${ex.getMessage}"))}
+  }
+
+  def rateMovie(movieId: String): Action[AnyContent] = Action {
+    implicit request =>
+    try {
+      val requestBody = request.body
+      val movieJsonObject = requestBody.asJson
+
+      // This type of JSON un-marshalling will only work
+      // if ALL fields are POSTed in the request body
+      val movieItem: Option[Movie] =
+      movieJsonObject.flatMap(
+        Json.fromJson[Movie](_).asOpt
+      )
+      val editMovie = dataRepository.rateMovie(movieId, movieItem.get)
+      if (editMovie.isEmpty || editMovie == None) throw new Exception("Movie is not exists.")
+      Created(Json.toJson(editMovie))}
+    catch {case ex: Exception => InternalServerError(Json.obj("code" -> INTERNAL_SERVER_ERROR, "message" -> s"Rate movie error : ${ex.getMessage}"))}
+  }
+}
