36c917ff47e1af932620161424467c0bba64ae14
Gurunath <bhuvi23389@gmail.com>
2022-05-19 12:35:33 +0100

    Added plot in the respository

--- app/repositories/MovieRepository.scala
+++ app/repositories/MovieRepository.scala
@@ -20,7 +20,7 @@ class MovieRepository {
     "tt1375666",
     "Inception",
     "https://imdb-api.com/images/original/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_Ratio0.6837_AL_.jpg",
-    "(2010)",
+    "Dom Cobb is a skilled thief, the absolute best in the dangerous art of extraction, stealing valuable secrets from deep within the subconscious during the dream state, when the mind is at its most vulnerable. Cobb's rare ability has made him a coveted player in this treacherous new world of corporate espionage, but it has also made him an international fugitive and cost him everything he has ever loved. Now Cobb is being offered a chance at redemption. One last job could give him his life back but only if he can accomplish the impossible, inception. Instead of the perfect heist, Cobb and his team of specialists have to pull off the reverse: their task is not to steal an idea, but to plant one. If they succeed, it could be the perfect crime. But no amount of careful planning or expertise can prepare the team for the dangerous enemy that seems to predict their every move. An enemy that only Cobb could have seen coming.",
     "PG-13",
     "4"
   )
